c0f02dc56e90708347e337302bc10021696310f6
Ankit yadav <ankityadav941318@gmail.com>
2026-01-25 17:08:36 +0530

    changes

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageList from "./ImageList";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Image Gallery</h1>
+      <ImageList />
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/ImageList.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function ImageList() {
+  const [images, setImages] = useState([]);
+
+  // 🔴 BACKEND LIVE URL YA LOCAL
+const API = "http://localhost:5000/api/image";
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    const res = await axios.get(`${API}/all`);
+    setImages(res.data);
+  };
+
+  const likeImage = async (id) => {
+    await axios.post(`${API}/like/${id}`);
+    fetchImages();   // refresh list
+  };
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      {images.map((img) => (
+        <div
+          key={img._id}
+          style={{
+            border: "1px solid gray",
+            margin: "10px",
+            padding: "10px",
+            width: "220px",
+          }}
+        >
+          <img
+            src={img.imageUrl}
+            alt={img.title}
+            width="200"
+            height="200"
+          />
+          <h3>{img.title}</h3>
+          <p>Likes: {img.likes}</p>
+          <button onClick={() => likeImage(img._id)}>❤️ Like</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ImageList;
